dd6ec7ac07f49dc85ab29a921bd61970d879cc0c
Horhe305 <101187054+Horhe305@users.noreply.github.com>
2022-03-22 11:46:40 +0000

    koniec

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner; 
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+  Scanner myObj = new Scanner(System.in);
+
+    System.out.println("H\ne\nl\nl\no\n\nw\no\nr\nl\nd");
+   
+}
 }
